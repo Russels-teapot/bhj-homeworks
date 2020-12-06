50f8f3cee68532a74aac63950b65c021fe59f3b6
Russels-teapot <baxarev.777@yandex.ru>
2020-12-06 15:43:42 +0300

    Частично выполнена домашняя работа

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,33 +1,31 @@
-const usersCart = document.querySelector('.cart__products')
-let productInCart = document.createElement('div')
-productInCart.classList.add('cart__product')
-let productImage = document.createElement('img')
-productImage.classList.add('cart__product-image')
-let productQuantity = document.createElement('div')
-productQuantity.classList.add('cart__product-count')
-productInCart.insertAdjacentElement("afterbegin", productImage)
-productInCart.insertAdjacentElement("beforeend", productQuantity)
-const productCards = Array.from(document.getElementsByClassName('product'))
-for (let productCard of productCards) {
-    let decButton = productCard.querySelector('.product__quantity-control_dec')
-    let incButton = productCard.querySelector('.product__quantity-control_inc')
-    let addingButton = productCard.querySelector('.product__add')
-    let imageInCart = productCard.querySelector('.product__image').getAttribute('src')
-    let productId = productCard.getAttribute('data-id')
-    console.log(addingButton);
-    console.log(imageInCart)
+const usersCart = document.querySelector('.cart__products');
+const productInCart = document.createElement('div');
+productInCart.classList.add('cart__product');
+const productImage = document.createElement('img');
+productImage.classList.add('cart__product-image');
+const productQuantity = document.createElement('div');
+productQuantity.classList.add('cart__product-count');
+productInCart.insertAdjacentElement("afterbegin", productImage);
+productInCart.insertAdjacentElement("beforeend", productQuantity);
+const productCards = Array.from(document.getElementsByClassName('product'));
+for (const productCard of productCards) {
+    const decButton = productCard.querySelector('.product__quantity-control_dec');
+    const incButton = productCard.querySelector('.product__quantity-control_inc');
+    const addingButton = productCard.querySelector('.product__add');
+    const imageInCart = productCard.querySelector('.product__image').getAttribute('src');
+    const productId = productCard.getAttribute('data-id');
     incButton.addEventListener('click', ()=> {
         let quantityValue = +productCard.querySelector('.product__quantity-value').textContent;
         if (quantityValue>=0) {
             productCard.querySelector('.product__quantity-value').textContent = (quantityValue + 1).toString()
     }
-    })
+    });
     decButton.addEventListener('click', ()=>{
         let quantityValue = +productCard.querySelector('.product__quantity-value').textContent;
         if (quantityValue>0) {
             productCard.querySelector('.product__quantity-value').textContent = (quantityValue - 1).toString()
         }
-    })
+    });
     addingButton.addEventListener('click', ()=>{
         usersCart.insertAdjacentElement("afterbegin", productInCart);
         productImage.setAttribute('src', imageInCart);
@@ -39,7 +37,7 @@ for (let productCard of productCards) {
         if(productQuantity.innerText === (0).toString()) {
             productInCart.parentNode.removeChild(productInCart)
         }
-    })
-    };
+    });
+    }
 
 
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -5,12 +5,11 @@ const itemToDo = document.createElement('div');
 itemToDo.classList.add('task');
 const itemTitle = document.createElement('div');
 itemTitle.classList.add('task__title');
-const deleteItem = document.createElement('a')
-deleteItem.classList.add('task__remove')
-deleteItem.setAttribute('href', '""')
-itemToDo.insertAdjacentElement('afterbegin', itemTitle)
-itemToDo.insertAdjacentElement('beforeend', deleteItem)
-console.log(itemToDo)
+const deleteItem = document.createElement('a');
+deleteItem.classList.add('task__remove');
+deleteItem.setAttribute('href', '""');
+itemToDo.insertAdjacentElement('afterbegin', itemTitle);
+itemToDo.insertAdjacentElement('beforeend', deleteItem);
 addingButton.addEventListener('click', ()=>{
     itemTitle.innerText = inputField.value
     taskList.insertAdjacentElement('afterbegin', itemToDo)
